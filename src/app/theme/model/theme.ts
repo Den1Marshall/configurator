e384755b75bfd7dfbdf9956f0d353758122ea448
Den Marshall <dank03052005@gmail.com>
2024-01-08 17:39:10 +0200

    feat: ukrainian localization for MUI

--- src/app/theme/model/theme.ts
+++ src/app/theme/model/theme.ts
@@ -1,27 +1,34 @@
 import { createTheme } from '@mui/material';
 import { Roboto } from 'next/font/google';
+import { ukUA } from '@mui/material/locale';
 
 const roboto = Roboto({
   subsets: ['latin'],
   weight: ['100', '300', '400', '500', '700', '900'],
 });
 
-export const lightTheme = createTheme({
-  palette: {
-    mode: 'light',
-  },
+export const lightTheme = createTheme(
+  {
+    palette: {
+      mode: 'light',
+    },
 
-  typography: {
-    fontFamily: roboto.style.fontFamily,
+    typography: {
+      fontFamily: roboto.style.fontFamily,
+    },
   },
-});
+  ukUA
+);
 
-export const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-  },
+export const darkTheme = createTheme(
+  {
+    palette: {
+      mode: 'dark',
+    },
 
-  typography: {
-    fontFamily: roboto.style.fontFamily,
+    typography: {
+      fontFamily: roboto.style.fontFamily,
+    },
   },
-});
+  ukUA
+);
