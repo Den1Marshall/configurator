0d4b1d7c4996574b92ba7c3169d9afedb2a76456
Den Marshall <dank03052005@gmail.com>
2024-01-29 20:48:56 +0200

    feat: bottom sheet semantics & positioning improvements

--- src/widgets/MobileHeader/ui/MobileHeader.tsx
+++ src/widgets/MobileHeader/ui/MobileHeader.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { FC, useRef } from 'react';
-import { AnimatedPaper } from '@/shared/ui';
+import { AnimatedPaper, Stack } from '@/shared/ui';
 import { SelectNumbersMobile } from '@/features/SelectNumbers';
 import { SelectLettersMobile } from '@/features/SelectLetters';
 import { Puller } from './Puller';
@@ -13,7 +13,7 @@ import { useTheme } from '@mui/material';
 import { ApproximateCost } from '@/entities/ApproximateCost';
 
 export const MobileHeader: FC = () => {
-  const detents = [95.7, 58.5, 10];
+  const detents = [85, 58.5, 10];
   const theme = useTheme();
 
   let position = detentToPx(detents[0]);
@@ -68,49 +68,58 @@ export const MobileHeader: FC = () => {
   );
 
   return (
-    <AnimatedPaper
-      ref={ref}
-      elevation={3}
-      style={{ y }}
-      {...bind()}
-      sx={{
-        zIndex: theme.zIndex.tooltip + 1,
-        position: 'fixed',
-        left: 0,
-        bottom: 0,
-        width: '100%',
-        height: '100%',
-        display: 'flex',
-        justifyContent: 'center',
-        px: 2,
-        pt: 10,
-        gap: 1,
-        touchAction: 'none',
-        mb: 'env(safe-area-inset-bottom)',
-      }}
-    >
-      <Puller
-        onClick={() => {
-          const [bottom, middle] = [
-            detentToPx(detents[0]),
-            detentToPx(detents[1]),
-          ];
-          if (position === middle) {
-            position = bottom;
-            api.start({ to: { y: position } });
-          } else {
-            position = middle;
-            api.start({ to: { y: middle } });
-          }
+    <header>
+      <AnimatedPaper
+        ref={ref}
+        elevation={3}
+        style={{ y }}
+        {...bind()}
+        sx={{
+          zIndex: theme.zIndex.tooltip + 1,
+          position: 'fixed',
+          left: 0,
+          bottom: 0,
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          px: 2,
+          pt: 6,
+          gap: 6,
+          touchAction: 'none',
+          mb: 'env(safe-area-inset-bottom)',
         }}
-      />
-      <SelectRegionMobile />
-      <SelectNumbersMobile />
-      <SelectLettersMobile />
-      <ApproximateCost
-        mobile
-        sx={{ top: '20%', left: '50%', transform: 'translate(-50%, -20%)' }}
-      />
-    </AnimatedPaper>
+      >
+        <Puller
+          onClick={() => {
+            const [bottom, middle] = [
+              detentToPx(detents[0]),
+              detentToPx(detents[1]),
+            ];
+            if (position === middle) {
+              position = bottom;
+              api.start({ to: { y: position } });
+            } else {
+              position = middle;
+              api.start({ to: { y: middle } });
+            }
+          }}
+        />
+        <Stack
+          direction={'row'}
+          alignItems={'center'}
+          width={'100%'}
+          spacing={1}
+        >
+          <SelectRegionMobile />
+          <SelectNumbersMobile />
+          <SelectLettersMobile />
+        </Stack>
+        <ApproximateCost
+          mobile
+          sx={{ position: 'relative', top: 0, left: 0, transform: 'none' }}
+        />
+      </AnimatedPaper>
+    </header>
   );
 };
