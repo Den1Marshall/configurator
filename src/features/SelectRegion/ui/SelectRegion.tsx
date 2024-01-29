0c8a7e54402aa8dbcbb09f13e0f8fe866da6fad2
Den Marshall <dank03052005@gmail.com>
2024-01-29 21:23:17 +0200

    feat: region search by it's code

--- src/features/SelectRegion/ui/SelectRegion.tsx
+++ src/features/SelectRegion/ui/SelectRegion.tsx
@@ -4,10 +4,12 @@ import { FC } from 'react';
 import { Autocomplete, TextField } from '@/shared/ui';
 import {
   LicensePlateRegion,
+  convertCyrillicToLatin,
   licensePlateRegionsArr,
   updateRegion,
 } from '@/entities/LicensePlate';
 import { SelectRegionPaper } from './SelectRegionPaper';
+import { createFilterOptions } from '@mui/material';
 
 export const SelectRegion: FC = () => {
   const dispatch = useAppDispatch();
@@ -16,10 +18,32 @@ export const SelectRegion: FC = () => {
     (state) => state.persistedLicensePlateReducer.region
   );
 
+  const options = createFilterOptions({
+    stringify: (option: LicensePlateRegion) =>
+      option.title + option.year + option.code,
+  });
+
   return (
     <Autocomplete
       PaperComponent={SelectRegionPaper}
       value={region}
+      filterOptions={(_, state) => {
+        if (
+          state.inputValue.length === 2 &&
+          licensePlateRegionsArr.find(
+            (reg) =>
+              reg.code ===
+              convertCyrillicToLatin(state.inputValue.toUpperCase())
+          )
+        ) {
+          state.inputValue = convertCyrillicToLatin(
+            state.inputValue.toUpperCase()
+          );
+          return options(_, state);
+        }
+
+        return options(_, state);
+      }}
       onChange={(_event: any, newValue: LicensePlateRegion | null) => {
         const newRegion = licensePlateRegionsArr.find(
           (region) => region.code === newValue?.code
