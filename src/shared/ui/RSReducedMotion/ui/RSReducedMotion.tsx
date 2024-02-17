2cc5f6b4699edfa4a4787086b6039dc5e62444e0
Den Marshall <dank03052005@gmail.com>
2024-02-17 19:27:36 +0200

    feat: skip react-spring animations if user prefers reduced motion

---
+++ src/shared/ui/RSReducedMotion/ui/RSReducedMotion.tsx
@@ -0,0 +1,8 @@
+'use client';
+import { FC } from 'react';
+import { useReducedMotion } from '@react-spring/web';
+
+export const RSReducedMotion: FC = () => {
+  useReducedMotion();
+  return null;
+};
