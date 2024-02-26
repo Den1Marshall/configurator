7d0a4404ec80416def5795612336322a81004364
Den Marshall <dank03052005@gmail.com>
2024-02-26 13:16:23 +0200

    refactor: simplify findClosestDetent func

--- src/widgets/MobileHeader/libs/findClosestDetent.ts
+++ src/widgets/MobileHeader/libs/findClosestDetent.ts
@@ -1,11 +1,4 @@
-export const findClosestDetent = (detents: number[], value: number): number => {
-  let closestNumber: number = detents[0];
-
-  detents.forEach((detent) => {
-    if (Math.abs(detent - value) < Math.abs(closestNumber - value)) {
-      closestNumber = detent;
-    }
-  });
-
-  return closestNumber;
-};
+export const findClosestDetent = (detents: number[], value: number): number =>
+  detents.reduce((prev, curr) =>
+    Math.abs(curr - value) < Math.abs(prev - value) ? curr : prev
+  );
