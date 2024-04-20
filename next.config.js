ba95b6078213052b32ae2012bc1057cc92aacb5c
Denys Hrychulevych <denyshrychulevych@gmail.com>
2024-04-20 17:53:21 +0300

    feat: migrate from next-pwa to ducanh next-pwa

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-const withPWA = require('next-pwa')({
+const withPWA = require('@ducanh2912/next-pwa').default({
   dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
 });
 
 module.exports = withPWA(nextConfig);
